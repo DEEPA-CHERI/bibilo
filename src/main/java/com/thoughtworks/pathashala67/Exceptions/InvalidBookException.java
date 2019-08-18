aa216bd4f0f7798f0277c5b0a83239627566510b
DEEPA <cherideepa@gmail.com>
2019-08-18 06:01:37 +0530

    [Deepa] Add. InvalidBookException

---
+++ src/main/java/com/thoughtworks/pathashala67/Exceptions/InvalidBookException.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.pathashala67.Exceptions;
+
+public class InvalidBookException extends Exception {
+   public InvalidBookException(String errorMessage)
+    {
+        super(errorMessage);
+    }
+}
