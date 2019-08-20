19051299566047a2c49ec5fb67903bde2a1fe348
DEEPA <cherideepa@gmail.com>
2019-08-20 15:27:10 +0530

    [Deepa] Add. load books and movies

--- src/test/java/IntializerTest.java
+++ src/test/java/IntializerTest.java
@@ -1,18 +1,25 @@
 import com.thoughtworks.pathashala67.model.Books;
 import com.thoughtworks.pathashala67.model.Intializer;
+import com.thoughtworks.pathashala67.model.Movies;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 class IntializerTest {
-    private Intializer intializer = mock(Intializer.class);
+    private Intializer intializer = mock( Intializer.class );
 
 
     @Test
     void expectBooksWhenLoadBooksMethodCalled() {
 
-        assertTrue(intializer.loadBooks()instanceof Books);
+        assertTrue( intializer.loadBooks() instanceof Books );
+    }
+
+    @Test
+    void expectMoviesWhenLoadMoviesMethodCalled() {
+
+        assertTrue( intializer.loadMovies() instanceof Movies );
     }
 
 
